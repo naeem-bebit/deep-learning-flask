484ab8b5c10d6e012707b1c388f36543ab034974
naeem-bebit <naeemhussien@gmail.com>
2021-09-13 23:16:52 +0800

    import keras

--- app_main.py
+++ app_main.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+from keras.preprocessing.image import load_image, img_to_array
+from keras.applications.vgg16 import preprocess_input, decode_predictions, VGG16
 
 app = Flask(__name__)
 
-@app.route('/', methods =['GET'])
-def hello_world():
-    return 'hello world'
+@app.route('/', methods = ['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods = ['POST'])
+def predict():
+    imagefile = request.files['imagefile']
+    imagefile.save(image_path = "./images/" + imagefile.filename)
+    return render_template('index.html', prediction = classification)
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
