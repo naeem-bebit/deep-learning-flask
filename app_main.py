c883154a017ac96c24151951596330ab308dfd9d
naeem-bebit <naeemhussien@gmail.com>
2021-09-14 23:26:35 +0800

    tensorflow

--- app_main.py
+++ app_main.py
@@ -3,6 +3,16 @@ from flask import Flask, render_template, request
 from keras.preprocessing.image import load_image, img_to_array
 from keras.applications.vgg16 import preprocess_input, decode_predictions, VGG16
 
+import tensorflow as tf
+from tensorflow import keras
+
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+model = MobileNetV2(weights='imagenet')
+
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
