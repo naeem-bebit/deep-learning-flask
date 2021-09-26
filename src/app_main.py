524cffb78fef440f280686bc12b7314e1e224a6d
naeem-bebit <naeemhussien@gmail.com>
2021-09-26 23:43:49 +0800

    predict post

--- src/app_main.py
+++ src/app_main.py
@@ -28,6 +28,25 @@ def model_predict(img_path, model):
 # @app.route('/predict', methods = ['POST'])
 @app.route('/predict', methods = ['GET','POST'])
 def predict():
+    if request.method == 'POST':
+    # Get the file from post request
+    f = request.files['file']
+
+    # Save the file to ./uploads
+    basepath = os.path.dirname(__file__)
+    file_path = os.path.join(
+        basepath, 'uploads', secure_filename(f.filename))
+    f.save(file_path)
+
+    # Make prediction
+    preds = model_predict(file_path, model)
+
+    # Process your result for human
+    # pred_class = preds.argmax(axis=-1)            # Simple argmax
+    pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+    result = str(pred_class[0][0][1])               # Convert to string
+    return result
+    # return None
 #     # imagefile = request.files['imagefile']
 #     # imagefile.save(image_path = "./images/" + imagefile.filename)
 #     # return render_template('index.html', prediction = classification)
